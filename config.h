0028d688a1c667083b05f9fc0631dfe9459547af
Chris Dean <chrisdean258@gmail.com>
2019-09-04 13:04:54 -0400

    ...

--- config.h
+++ config.h
@@ -50,7 +50,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.60; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
@@ -130,6 +130,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_v,      spawn,          SHCMD("vol -5%") },
 	{ MODKEY|ShiftMask,             XK_v,      spawn,          SHCMD("vol +5%") },
 	{ MODKEY,                       XK_a,      spawn,          SHCMD("dmenu_audio") },
+	{ MODKEY|ShiftMask,             XK_b,      spawn,          SHCMD("dmenu_backlight") },
 };
 
 /* button definitions */
