b7474a896da1f1e48f6ca3c8e5356d241b189255
Chris Dean <chrisdean258@gmail.com>
2019-11-24 23:18:17 -0500

    control c to kill client

--- config.h
+++ config.h
@@ -102,7 +102,7 @@ static Key keys[] = {
 	{ 0,                 XK_l,      setmfact,       {.f = +0.05} },
 	{ 0,                 XK_Tab,    view,           {0} },
 	{ 0,                 XK_Escape, norm,           {0} },
-	{ ShiftMask,         XK_c,      killclient,     {0} },
+	{ ControlMask,       XK_c,      killclient,     {0} },;
 	{ 0,                 XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ 0,                 XK_m,      setlayout,      {.v = &layouts[1]} },
 	{ 0,                 XK_s,      setlayout,      {.v = &layouts[2]} },
