6d177fe2992529d87f63d7a86a71f2ec56cda467
Chris Dean <chrisdean258@gmail.com>
2020-02-04 14:43:39 -0500

    config update

--- config.h
+++ config.h
@@ -85,55 +85,56 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_t,      spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_Return, handle_st,      {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ControlMask,           XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_y,      setmfact,       {.f = -0.05} },
-	{ MODKEY|ShiftMask,             XK_y,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,                       XK_Escape, killclient,     {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = 0 } },
-	{ MODKEY|ControlMask,           XK_0,      toggleview,     {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  focustagmon,    {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, focustagmon,    {.i = +1 } },
-	{ MODKEY|ControlMask,           XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_g,      handle_browser, SHCMD("browser") },
-	{ MODKEY,                       XK_o,      spawn,          SHCMD("monitor detect") },
-	{ MODKEY,                       XK_r,      restart,        {0} },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY,                       XK_v,      spawn,          SHCMD("vol -5%") },
-	{ MODKEY|ShiftMask,             XK_v,      spawn,          SHCMD("vol +5%") },
-	{ MODKEY,                       XK_a,      spawn,          SHCMD("dmenu_audio") },
-	{ MODKEY|ShiftMask,             XK_b,      spawn,          SHCMD("dmenu_backlight") },
-	{ MODKEY|ShiftMask,             XK_u,      spawn,          SHCMD("backlight +10") },
-	{ MODKEY,                       XK_u,      spawn,          SHCMD("backlight -10") },
+	/* modifier                     key           function        argument */
+	{ MODKEY,                       XK_p,         spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_semicolon, spawn,          SHCMD("dmenu_runb") },
+	{ MODKEY|ShiftMask,             XK_t,         spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return,    handle_st,      {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return,    spawn,          {.v = termcmd } },
+	{ MODKEY|ControlMask,           XK_Return,    zoom,           {0} },
+	{ MODKEY,                       XK_b,         togglebar,      {0} },
+	{ MODKEY,                       XK_j,         focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,         focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,         incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,         incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_y,         setmfact,       {.f = -0.05} },
+	{ MODKEY|ShiftMask,             XK_y,         setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Tab,       view,           {0} },
+	{ MODKEY,                       XK_Escape,    killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_c,         killclient,     {0} },
+	{ MODKEY,                       XK_t,         setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_m,         setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_f,         setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,     setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,     togglefloating, {0} },
+	{ MODKEY,                       XK_0,         view,           {.ui = 0 } },
+	{ MODKEY|ControlMask,           XK_0,         toggleview,     {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,         tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,     focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period,    focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,     focustagmon,    {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,    focustagmon,    {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_comma,     tagmon,         {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_period,    tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_g,         handle_browser, SHCMD("browser") },
+	{ MODKEY,                       XK_o,         spawn,          SHCMD("monitor detect") },
+	{ MODKEY,                       XK_r,         restart,        {0} },
+	TAGKEYS(                        XK_1,                         0)
+	TAGKEYS(                        XK_2,                         1)
+	TAGKEYS(                        XK_3,                         2)
+	TAGKEYS(                        XK_4,                         3)
+	TAGKEYS(                        XK_5,                         4)
+	TAGKEYS(                        XK_6,                         5)
+	TAGKEYS(                        XK_7,                         6)
+	TAGKEYS(                        XK_8,                         7)
+	TAGKEYS(                        XK_9,                         8)
+	{ MODKEY|ShiftMask,             XK_q,         quit,           {0} },
+	{ MODKEY,                       XK_v,         spawn,          SHCMD("vol -5%") },
+	{ MODKEY|ShiftMask,             XK_v,         spawn,          SHCMD("vol +5%") },
+	{ MODKEY,                       XK_a,         spawn,          SHCMD("dmenu_audio") },
+	{ MODKEY|ShiftMask,             XK_b,         spawn,          SHCMD("dmenu_backlight") },
+	{ MODKEY|ShiftMask,             XK_u,         spawn,          SHCMD("backlight +10") },
+	{ MODKEY,                       XK_u,         spawn,          SHCMD("backlight -10") },
 };
 
 /* button definitions */
