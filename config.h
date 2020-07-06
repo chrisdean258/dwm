5c06c3ece578763550610490f693e24cdae7c49a
Chris Dean <chrisdean258@gmail.com>
2020-07-06 14:04:35 -0400

    gimp in config

--- config.h
+++ config.h
@@ -49,8 +49,8 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class               instance    title       tags mask     isfloating   monitor */
-	{ "hromsdfsd",         NULL,       NULL,       1 << 0,       0,           -1 },
+	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
